ea4c9b4a1ae88f3a03230e247fbaab9d9df2979f
eladbenmoshe <50212564+eladbenmoshe@users.noreply.github.com>
2019-06-10 23:03:02 -0700

    Create file.c

---
+++ file.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+//
+// simple file output
+//
+int main(int arc, char* argv[])
+{
+  int arr[] = { 5, 8, 3, 7, 0 };
+
+  FILE* outFile;
+
+
+  // open a file
+  outFile = fopen("output.txt", "w");
+  if (outFile == NULL) {
+    printf("error - failed to open file for writing\n");
+    return 1;
+  }
+
+  fprintf(outFile, "This is a test file\n");
+
+  int i = 0;
+  while (arr[i] != 0) {
+    fprintf(outFile, "Val %d = %d\n", i, arr[i]);
+    i++;
+  }
+  
+  fclose(outFile);
+}
