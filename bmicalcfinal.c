9d5979674ce9ab357c462fa11b002e4450ef7315
eladbenmoshe <50212564+eladbenmoshe@users.noreply.github.com>
2019-06-10 23:01:15 -0700

    Create bmicalcfinal.c

---
+++ bmicalcfinal.c
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>BMI Calculator</title>
+<style>
+body {background-color: white;}
+h1   {color: black;}
+p    {
+        color: purple;
+        font-family: courier;
+        font-size: 120%
+}
+img {
+    width:50%;
+}
+</style>
+</head>
+<body>
+
+<p>
+
+<a href= "index.html"> Back to Home Page</a>
+
+ <h1>BMI Calculator</h1>
+
+<embed src="output.txt" width="1000"  height="1000">
+
+
+</body>
+</html>
